aa5cee42ebfac416ba956f4858b59234b0aa4d71
Antonio <antoniogviegas@hotmail.com>
2021-06-17 22:05:16 +0200

    Adjust point of view

--- landing-page/app.js
+++ landing-page/app.js
@@ -57,6 +57,7 @@ scene.add(axes);
 //Creates the orbit controls (to navigate the scene)
 const controls = new OrbitControls(camera, threeCanvas);
 controls.enableDamping = true;
+controls.target.set(-4, 0, 0);
 
 //Animation loop
 const animate = () => {
