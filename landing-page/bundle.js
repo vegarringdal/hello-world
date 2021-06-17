20f0674f327dcee2896e5393a0b0c09d5e35464c
Antonio <antoniogviegas@hotmail.com>
2021-06-17 22:13:07 +0200

    Adjust point of view

--- landing-page/bundle.js
+++ landing-page/bundle.js
@@ -71397,7 +71397,6 @@ scene.add(ambientLight);
 
 const directionalLight = new DirectionalLight(lightColor, 1);
 directionalLight.position.set(0, 10, 0);
-directionalLight.target.position.set(-4.5, 0, 0);
 scene.add(directionalLight);
 scene.add(directionalLight.target);
 
