8560dc661b69a1f7bc39fd2207a390568ea94ce3
Antonio <antoniogviegas@hotmail.com>
2021-06-17 22:25:42 +0200

    Adjust camera position

--- landing-page/app.js
+++ landing-page/app.js
@@ -57,7 +57,7 @@ scene.add(axes);
 //Creates the orbit controls (to navigate the scene)
 const controls = new OrbitControls(camera, threeCanvas);
 controls.enableDamping = true;
-controls.target.set(-3, 0, 0);
+controls.target.set(-2, 0, 0);
 
 //Animation loop
 const animate = () => {
